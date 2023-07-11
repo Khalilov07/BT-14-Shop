0d3d3c2e74b62b8c42cbd7945abc543f2e93425e
Marka <khalilovakram07@gmail.com>
2023-07-11 15:30:43 +0600

    add about page and add start markup

---
+++ src/pages/About/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './aboutpage.module.css'
+
+const AboutPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AboutPage;
